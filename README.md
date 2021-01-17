2af671c8cea659e316efdd66ad0eeddf613f29e1
Charlys Pinheiro <charlys.pinheiro@hp.com>
2021-01-16 22:08:01 -0300

    Versonando projeto calculadora

---
+++ README.md
@@ -0,0 +1,7 @@
+## Comando
+
+### Criar projeto react
+
+*npm i -g create-react-app*
+
+*create-react-app [nome_projeto]*
---
+++ src/main/Calculator.jsx
@@ -0,0 +1,121 @@
+import React, { Component } from 'react'
+import Button from '../components/Button'
+import './Calculator.css'
+import Display  from "../components/Display";
+
+const initicalState = {
+    displayValue : '0',
+    clearDisplay : false,
+    operation: null,
+    values: [0, 0],
+    current: 0
+}
+
+export default class Calculator extends Component {
+
+    state = { ...initicalState}
+
+    //assim resollve o problema do bind no this.
+    constructor(props){
+        super(props)
+        this.clearMemory = this.clearMemory.bind(this)
+    }
+
+    clearMemory(){
+        this.setState({...initicalState})
+    }
+
+    realzarOperacao = (op, values) => {
+        switch (op) {
+            case '+':
+                values[0] = values[0] + values[1]
+                break;
+            case '-':
+                values[0] = values[0] - values[1]
+                break;
+            case '*':
+                values[0] = values[0] * values[1]
+                break;
+            default:
+                values[0] = values[0] / values[1]
+                break;
+        }
+        return values
+    }
+
+    setOperation(operation){
+        if(this.state.current === 0){
+            this.setState({operation, current: 1, clearDisplay: true })
+        } else{
+            const equals = operation === '='
+            const currentOperation = this.state.operation
+
+            const values = [ ...this.state.values]
+
+            /* Forma que pode causar problema
+            try {
+                values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`) 
+            } catch(e){
+                values[0] = this.state.values[0]
+            }*/
+
+            this.realzarOperacao(currentOperation, values)
+            
+            values[1] = 0
+
+            this.setState({
+                displayValue: values[0],
+                operation : equals ? null : operation,
+                current : equals ? 0 : 1,
+                clearDisplay : !equals,
+                values
+            })
+        }
+    }
+
+    addDigit(n){
+        if (n === '.' && this.state.displayValue.includes('.')){
+            return
+        }
+
+        const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
+        const currentValue = clearDisplay ? '' : this.state.displayValue
+        const displayValue = currentValue + n
+        this.setState({ displayValue, clearDisplay:false })
+
+        if(n !== '.'){
+            const i = this.state.current
+            const newValue = parseFloat(displayValue)
+            const values = [ ...this.state.values]
+            values[i] = newValue
+            this.setState({ values })
+        }
+    }
+
+    render(){
+        const addDigit = (n) => {this.addDigit(n)} //assim resollve o problema do bind no this.
+        const setOperation = (op) => {this.setOperation(op)}
+        return (
+            <div className="calculator">
+                <Display value={this.state.displayValue}></Display>
+                <Button label='AC' click={this.clearMemory} triple/>
+                <Button label='/' click={setOperation} operation/>
+                <Button label='7' click={addDigit}/>
+                <Button label='8' click={addDigit}/>
+                <Button label='9' click={addDigit}/>
+                <Button label='*' click={setOperation} operation/>
+                <Button label='4' click={addDigit}/>
+                <Button label='5' click={addDigit}/>
+                <Button label='6' click={addDigit}/>
+                <Button label='-' click={setOperation} operation/>
+                <Button label='1' click={addDigit}/>
+                <Button label='2' click={addDigit}/>
+                <Button label='3' click={addDigit}/>
+                <Button label='+' click={setOperation} operation/>
+                <Button label='0' click={addDigit} double/>
+                <Button label='.' click={addDigit}/>
+                <Button label='=' click={setOperation} operation/>
+            </div>
+        )
+    }
+}
